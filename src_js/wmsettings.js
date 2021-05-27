206a4ec86b62d50b6be99268cad8790eff25f953
Evgeny Blokhin <eb@tilde.pro>
2021-05-27 15:19:23 +0200

    Re-organize the repo

--- src_js/wmsettings.js
+++ src_js/wmsettings.js
@@ -294,7 +294,7 @@ wmgui.editions = {
         var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
         link.type = 'image/x-icon';
         link.rel = 'shortcut icon';
-        link.href = '/asm.ico';
+        link.href = '/editions/asm/asm.ico';
         document.getElementsByTagName('head')[0].appendChild(link);
     }}
 };
