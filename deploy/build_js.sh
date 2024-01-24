2164cbb899303b6d3e33db717039bd07ee2e1492
Evgeny Blokhin <eb@tilde.pro>
2024-01-24 19:23:51 +0100

    Fix compilation bug & polish

--- deploy/build_js.sh
+++ deploy/build_js.sh
@@ -4,8 +4,7 @@ ROOT=$(dirname $0)/../
 JSCOMP=$ROOT/third_party/jscomp/compiler.jar
 JSTARGET=$ROOT/ermac.min.js
 
-# Desktop (not mobile) GUI
-java -jar $JSCOMP --jscomp_off checkTypes --language_in ECMASCRIPT5_STRICT --js $ROOT/src_js/browser_chk.js >> $JSTARGET
+java -jar $JSCOMP --jscomp_off checkTypes --language_in ECMASCRIPT5_STRICT --js $ROOT/src_js/browser_chk.js > $JSTARGET
 
 java -jar $JSCOMP --jscomp_off checkTypes --language_in ECMASCRIPT5_STRICT --js $ROOT/src_js/third_party/jquery.tablesorter.js --compilation_level WHITESPACE_ONLY >> $JSTARGET
 java -jar $JSCOMP --jscomp_off checkTypes --language_in ECMASCRIPT5_STRICT --js $ROOT/src_js/third_party/wNumb.js >> $JSTARGET
